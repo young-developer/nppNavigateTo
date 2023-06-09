bce997b389cd99455466a297400e27da6a52bdad
Oleksii Maryshchenko <oleksii.maryshchenko@gmail.com>
2023-06-09 10:29:36 +0200

    Version 2.5.4

--- NppNavigateTo/Properties/AssemblyInfo.cs
+++ NppNavigateTo/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.5.2.0")]
-[assembly: AssemblyFileVersion("2.5.2.0")]
+[assembly: AssemblyVersion("2.5.4.0")]
+[assembly: AssemblyFileVersion("2.5.4.0")]
