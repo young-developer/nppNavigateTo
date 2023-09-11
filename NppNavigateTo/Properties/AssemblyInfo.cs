eafa43f7382e94a3554734f2056b5bfbda2ab6dd
Oleksii Maryshchenko <oleksii.maryshchenko@gmail.com>
2023-09-11 14:07:01 +0200

    v.2.7.0 bump

--- NppNavigateTo/Properties/AssemblyInfo.cs
+++ NppNavigateTo/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.5.1")]
-[assembly: AssemblyFileVersion("2.6.5.1")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
